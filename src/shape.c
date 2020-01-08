d5018eec2f12dc9a80e8b600146ca30fcd5e2a30
Michael O'Donnell <mo14776@my.bristol.ac.uk>
2020-01-08 11:37:41 +0000

    Refactor code to use an I/O interface

--- src/shape.c
+++ src/shape.c
@@ -1,44 +1,150 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <math.h>
 #include "sphere.h"
 #include "triangle.h"
 #include "disc.h"
+#include "chevron.h"
+
+void mosh_dda_io_cmd_read_in(int argc, char *argv[], char **outputFileName, char** shape, int **dimensions, double *externalSlope,double *internalSlope, double *radius,double *offset);
+int call_shape_function(FILE *outputFile, char* shape, int *dimensions, double externalSlope, double internalSlope,  double radius,double offset);
+int *get_dimensions(int dims[3]);
+
+
 int main(int argc, char *argv[])
 {
-   // printf("Begin\n");
 
-    FILE *sphere1=fopen("../bin/spherepositions.txt","w");
-	FILE *sphere2;
-//	FILE *disc1=fopen("../bin/discpositions.txt","w");
-//	FILE *disc2=fopen("../bin/discpositions2.txt","w");
-
-
-	int maxDimensions[3] = {1,1,1};
-    int numOfParticles=1;
+	int *dimensions=NULL;
     double radius=1.5;
-	int distance = 30;
-	double thickness = 5;
-	if(argc>1)
-	sphere2=fopen(argv[1],"w");
-	if(argc>2)
-    maxDimensions[0]=strtol(argv[2],NULL,10);
-	if(argc>3)
-    maxDimensions[1]=strtol(argv[3],NULL,10);
-	if(argc>4)
-    maxDimensions[2]=strtol(argv[4],NULL,10);
-	if(argc>5)
-	radius=strtod(argv[5],NULL);
-	if(argc>6)
-	numOfParticles=strtod(argv[6],NULL);
-	if(argc>7)
-	distance=strtod(argv[7],NULL);
+	double externalSlope=sqrt(3.0);
+	double internalSlope=sqrt(3.0);
+	double offset=0;
+	char *outputFileName=NULL;
+	char *shape=NULL;
 
-	//void sphere(int radius, int maxDimensions[3],int distance,int numOfParticles, FILE *output, FILE *output2);
-	//sphere(radius,maxDimensions,distance, numOfParticles, sphere1, sphere2);
-//	disc(radius, thickness,maxDimensions,distance, numOfParticles, disc1, disc2);
+	printf("Reading inputs\n");
+ 	mosh_dda_io_cmd_read_in(argc, argv, &outputFileName,&shape, &dimensions, &externalSlope,&internalSlope, &radius,&offset);
+	printf("Critical values\n");
+	printf("Dimensions: %d %d %d\n",dimensions[0],dimensions[1],dimensions[2]);
+	printf("Radius: %g\n",radius);
+	printf("External Slope: %g\n",externalSlope);
+	printf("Internal Slope: %g\n",internalSlope);
+	printf("offset: %g\n",offset);
 
-	double slope=sqrt(3.0);
- 	triangle(maxDimensions, numOfParticles, slope, thickness, sphere1);
+
+	printf("Opening file %s\n",outputFileName);
+	FILE *outputFile = fopen(outputFileName,"w");
+	if(outputFile==NULL)
+	return EXIT_FAILURE;
+
+	printf("Finding shape...\n");
+	if(call_shape_function(outputFile, shape, dimensions, externalSlope,internalSlope,  radius,offset) != 0)
+	printf("no shape name recognised\n");
+	else
+	printf("Shape complete\n");
+
+	fclose(outputFile);
+
     return 0;
+}
+
+
+void mosh_dda_io_cmd_read_in(int argc, char *argv[], char **outputFileName, char** shape, int **dimensions, double *externalSlope,double *internalSlope, double *radius,double *offset)
+{
+	int dims[3];
+
+	for(int i = 1; i <argc; i ++)
+	{
+		char *input = NULL;
+		if((input =strstr(argv[i],"--outputfile="))!=NULL)
+		{
+			input=strtok(input,"=");
+			input=strtok(NULL,"=");
+			*outputFileName = input;
+		}
+		else if((input =strstr(argv[i],"-shape"))!=NULL)
+		{
+			i++;
+			if(i!=argc)
+			*shape=argv[i];
+		}
+		else if((input =strstr(argv[i],"-dimensions"))!=NULL)
+		{
+			for(int j=0;j<3;j++)
+			{
+				i++;
+				if(i==argc)
+				break;
+
+				dims[j]=(int)strtol(argv[i],NULL,10);
+			}
+		}
+		else if((input =strstr(argv[i],"-externalslope"))!=NULL)
+		{
+			i++;
+			if(i!=argc)
+			*externalSlope=strtod(argv[i],NULL);
+		}
+		else if((input =strstr(argv[i],"-internalslope"))!=NULL)
+		{
+			i++;
+			if(i!=argc)
+			*internalSlope=strtod(argv[i],NULL);
+		}
+		else if((input =strstr(argv[i],"-radius"))!=NULL)
+		{
+			i++;
+			if(i!=argc)
+			*radius=strtod(argv[i],NULL);
+		}
+		else if((input =strstr(argv[i],"-offset"))!=NULL)
+		{
+			i++;
+			if(i!=argc)
+			*offset=strtod(argv[i],NULL);
+		}
+	}
+	*dimensions = get_dimensions(dims);
+}
+
+int call_shape_function(FILE *outputFile, char* shape, int *dimensions, double externalSlope,double internalSlope,  double radius, double offset)
+{
+	if(strcmp(shape,"sphere") == 0)
+	{
+		printf("found sphere\n");
+		sphere(dimensions,radius,outputFile);
+		return 0;
+	}
+	else if(strcmp(shape,"triangle") == 0)
+	{
+		printf("found triangle\n");
+		triangle(dimensions, externalSlope, outputFile);
+		return 0;
+	}
+	else if(strcmp(shape,"disc") == 0)
+	{
+		printf("found disc\n");
+		disc(dimensions, radius, outputFile);
+		return 0;
+	}
+	else if(strcmp(shape,"chevron") == 0)
+	{
+		printf("found chevron\n");
+		chevron(dimensions, externalSlope,internalSlope,offset, outputFile);
+		return 0;
+	}
+	else
+	{
+		return 1;
+	}
+}
+
+int *get_dimensions(int dims[3])
+{
+	int *dimensions = calloc(3,sizeof(*dimensions));
+	for(int i=0;i<3;i++)
+	dimensions[i]=dims[i];
+
+	return dimensions;
 }
